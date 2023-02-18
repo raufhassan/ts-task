9fb4e910681e17dbfdea88a9e01aa49c8b7cebed
raufhassan <raufhassan41@gmail.com>
2023-02-18 23:21:24 +0500

    initial configuration and form field page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import client from '@/graphql/config'
 import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  <ApolloProvider client={client}>
+        <Component {...pageProps} />
+  </ApolloProvider>
 }
